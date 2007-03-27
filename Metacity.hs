e647504db15ae264e3efbf5c3447a9084007b110
John Goerzen <jgoerzen@complete.org>
2007-03-27 10:42:08 -0500

    Work on finishing up Metacity.hs

--- Metacity.hs
+++ Metacity.hs
@@ -11,19 +11,11 @@ import Data.List
 getMacroBindings :: IO [(String, String)]
 getMacroBindings = do
     gc <- gconfGetDefault
-    commands' <- gconfAllEntries gc "/apps/metacity/keybinding_commands"
-    bindings' <- gconfAllEntries gc "/apps/metacity/global_keybindings"
+    bindings <- getBindings gc
+    commands <- getCommands gc
 
     -- Convert both commands and bindings to ("command_x", value)
     -- also keep disabled/empty ones out of the list.
-    let commands = filter (isPrefixOf "command_" . fst) . 
-                   filter ((/=) "" . snd) .
-                   gc2str $ commands'
-    let bindings = map (\(name, val) -> (drop 4 name, val)) .
-          filter (isPrefixOf "run_command_" . fst) . 
-          filter ((/=) "disabled" . snd) .
-          gc2str $ bindings'
-    
     return . combine bindings . 
        map (\(x, y) -> (x, drop (length "gmacroplay ") y)) .
        filter (isPrefixOf "gmacroplay " . snd) $
@@ -32,12 +24,53 @@ getMacroBindings = do
     where gc2str [] = []
           gc2str ((x, GConfValueString s):xs) = (x, s):gc2str xs
           gc2str (_:xs) = gc2str xs
-{-
+
+getBindings gc =
+    do bindings <- gconfAllEntries gc "/apps/metacity/global_keybindings"
+       return $ map (\(name, val) -> (drop 4 name, val)) .
+          filter (isPrefixOf "run_command_" . fst) . 
+          filter ((/=) "disabled" . snd) .
+          gc2str $ bindings
+    
+
+getCommands gc =
+    do commands <- gconfAllEntries gc "/apps/metacity/keybinding_commands"
+       return $ filter (isPrefixOf "command_" . fst) . 
+                   filter ((/=) "" . snd) .
+                   gc2str $ commands
+
 {- | Binds a new macro. -}
 bindMacro :: String                         -- ^ macro name
           -> String                         -- ^ keyboard shortcut
           -> IO ()
+bindMacro name shortcut = 
+    do gc <- gconfGetDefault
+       bindings <- getBindings gc
+       commands <- getCommands gc
+       command <- findCommand bindings commands
+       gconfSet gc ("/apps/metacity/global_keybindings/run_" ++ command)
+           shortcut
+       gconfSet gc ("/apps/metacity/keybinding_commands/" ++ command)
+           "gmacroplay " ++ name
+    where findCommand [] _ = error "Couldn't find available binding"
+          findCommand _ [] = error "Couldn't find available command"
+          findCommand ((bcmd,bshort):xs) commands =
+              if bshort == "disabled"
+                 then case lookup bcmd commands of
+                           Just "" -> bcmd
+                           _ -> findCommand xs commands
+                 else findCommand xs commands
 
 {- | Remove binding.  Takes macro name. -}
 removeBinding :: String -> IO ()
--}
+removeBinding name =
+    do gc <- gconfGetDefault
+       bindings <- getBindings gc
+       commands <- getCommands gc
+       let (cmd, _) = find ((==) ("gmacroplay " ++ name) . snd) commands
+       case cmd of
+            Nothing -> fail "Couldn't find command to unbind"
+            Just x -> do gconfSet gc ("/apps/metacity/global_keybindings/run_"
+                                      ++ x) "disabled"
+                         gconfSet gc ("/apps/metacity/keybinding_commands/" 
+                                      ++ x) ""
