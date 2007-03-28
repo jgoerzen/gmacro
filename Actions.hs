4605fe7daa27998f1b7c651761f4cf6edbc8e8c0
John Goerzen <jgoerzen@complete.org>
2007-03-28 12:43:59 -0500

    Started work on macro recording

--- Actions.hs
+++ Actions.hs
@@ -7,12 +7,16 @@ import Graphics.UI.Gtk.Glade
 import Buttons
 import MacroList
 import qualified Metacity
+import System.IO
+import Control.Concurrent
+import Control.Concurrent.MVar
+import System.Process
 
 initActions b list model macdir window xml = do
     onClicked (closebt b) (widgetDestroy window)
     onClicked (connectbt b) (connect list model macdir)
     onClicked (disconnectbt b) (disconnectMacro list model macdir)
-    onClicked (newbt b) (record xml)
+    onClicked (newbt b) (record model macdir xml)
 
 connect list model macdir = do
     items <- getSelectedItems list model
@@ -26,15 +30,32 @@ disconnectMacro list model macdir = do
     loadList model macdir
     where remove (name, _) = Metacity.removeBinding name
 
-record xml = do
+record model macdir xml = do
     recordwin <- xmlGetWidget xml castToWindow "recording"
     finishedbt <- xmlGetWidget xml castToButton "recdonebt"
 
+    (recordfn, recordh) <- openTempFile macdir "new-"
+    (_, macro, _, ph) <- runInteractiveCommand 
+       "xmacrorec2 -k 0xffff | egrep -v '^(ButtonRelease|ButtonPress|MotionNotify)' 2>/dev/null" 
+    -- Copy the data in a separate thread so we don't interfere with GUI
+    mv <- newEmptyMVar
+    forkIO $ do hSetBuffering macro NoBuffering
+                macroc <- hGetContents macro
+                hPutStr recordh macroc
+                putMVar mv ()
+    
     -- Intercept the click of the close button.  Don't let it destroy
     -- the widget (we'll need it again for the next recording)
-    onDelete recordwin (\_ -> recorddone recordwin >> return True)
-    onClicked finishedbt (recorddone recordwin)
+    onDelete recordwin (\_ -> recorddone recordwin macro ph recordh mv >> 
+                              return True)
+    onClicked finishedbt (recorddone recordwin macro ph recordh mv)
     windowPresent recordwin
     return ()
-    where recorddone recordwin = widgetHide recordwin
+    where recorddone recordwin macro ph recordh mv = do
+              terminateProcess ph
+              takeMVar mv
+              widgetHide recordwin
+              hClose macro
+              hClose recordh
+              loadList model macdir
 
